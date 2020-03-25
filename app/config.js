876f9f5801007397851e068e0a79c63c4894e8f6
melodycurtis <melodycurtis123@gmail.com>
2020-03-25 11:58:03 +0000

    starting new project for lrs application

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Learning Records Service application ',
+  serviceName: 'Apply to access the Learning Records Service ',
 
   // Default port that prototype runs on
   port: '3000',
