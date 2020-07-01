6218a5af4249422d25a4f63b1bece8807b2c05b6
melodycurtis <melodycurtis123@gmail.com>
2020-07-01 16:48:49 +0100

    edits from Natalie

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,3 +10,20 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+
+$('#continue').click(function () { 
+  if ($('#state-funded-school').is(':checked')) { location.href = '/prototypes/ur-school/location' }
+  if ($('#independent-school').is(':checked')) { location.href = '/prototypes/ur-school/location' }
+  if ($('#fe-sixthform').is(':checked')) { location.href = '/prototypes/ur-school/location' }
+  if ($('#itp').is(':checked')) { location.href = '/prototypes/ur-school/location' }
+  if ($('#he').is(':checked')) { location.href = '/prototypes/ur-school/location' }
+  if ($('#la').is(':checked')) { location.href = '/prototypes/ur-school/location' }
+  if ($('#careers').is(':checked')) { location.href = '/prototypes/ur-school/location' }
+  if ($('#charity').is(':checked')) { location.href = '/prototypes/ur-school/charity' }
+  if ($('#other').is(':checked')) { location.href = '/prototypes/ur-school/location' }
+  if ($('#other2').is(':checked')) { location.href = '/prototypes/ur-school/location' }
+
+ });
+
+
